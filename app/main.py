4d6cc745755d85a5af1be71de19edf16526c1bb1
Baonguyen1711 <Baonguyen17112004@gmail.com>
2026-01-10 10:49:27 +0700

    fix healthcheck

--- app/main.py
+++ app/main.py
@@ -120,7 +120,7 @@ async def dispatch(request: Request, call_next):
     # Skip middleware for preflight OPTIONS requests
     if request.method == "OPTIONS":
         return await call_next(request)
-    if request.url.path in ["/api/auth/token", "/api/room/validate", "/api/room/info", "/api/auth/verify","/api/room/spectator/join", "/docs", "/openapi.json", "/redoc"]:
+    if request.url.path in ["/health","/api/auth/token", "/api/room/validate", "/api/room/info", "/api/auth/verify","/api/room/spectator/join", "/docs", "/openapi.json", "/redoc"]:
         return await call_next(request)
 
     
